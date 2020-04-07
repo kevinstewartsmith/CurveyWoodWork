c83dfe6f2d6fe27c02e08b366c4555b6c0313b39
kssmith9 <kssmith9@ncsu.edu>
2020-04-07 23:03:07 +0700

    Initial Commit

---
+++ CurveyWoodWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurveyWoodWork
+//
+//  Created by Kevin Smith on 4/7/20.
+//  Copyright © 2020 Kevin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
